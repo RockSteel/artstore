ccccdd0af0f223caebc2fd1785bd0668d5c20d72
RockSteel <rocksteel2015@gmail.com>
2016-06-05 21:36:02 -0600

    Finish add product to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,20 @@ class ApplicationController < ActionController::Base
   def admin_required
     redirect_to "/" unless current_user.admin?
   end
-  
+
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+    def find_cart
+      cart = Cart.find_by(id: session[:cart_id])
+      cart = Cart.create unless cart.present?
+      session[:cart_id] = cart.id
+      cart
+    end
+
 end
