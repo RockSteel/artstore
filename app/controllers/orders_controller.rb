c709c045360531090db70c85ef4e8dff3023d7d0
rocksteel <rocksteel2015@gmail.com>
2016-06-15 10:04:41 +0800

    Fix mailer and pay2go for heroku

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,31 @@ class OrdersController < ApplicationController
 
   protect_from_forgery except: :pay2go_cc_notify
 
+  def pay2go_cc_notify
+    @order = Order.find_by_token(params[:id])
+
+    if params["Status"] == "SUCCESS"
+
+      @order.make_payment!
+
+      if @order.is_paid?
+        flash[:notice] = "信用卡付款成功"
+        redirect_to account_orders_path
+      else
+        render text: "信用卡失敗"
+      end
+    else
+      render text: "交易失敗"
+    end
+  end
+
   def create
     @order = current_user.orders.build(order_params)
     if @order.save
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
       current_cart.clean!
-      OrderMailer.notify_order_placed(@order).deliver!
+      #OrderMailer.notify_order_placed(@order).deliver!
       redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'still-waters-16687.herokuapp.com' }
+
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    #Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
 end
